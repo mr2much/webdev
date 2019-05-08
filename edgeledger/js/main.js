22201e6ff5a4c5413346aec3fa0013ebac4848bd
Anubis <anubis.lockward@gmail.com>
2019-05-08 06:02:05 -0400

    Added smooth scrolling

--- edgeledger/js/main.js
+++ edgeledger/js/main.js
@@ -12,3 +12,26 @@ function initMap() {
   // The marker, positioned at location
   const marker = new google.maps.Marker({ position: loc, map: map });
 }
+
+// Navbar menu background
+window.addEventListener("scroll", function() {
+  if (window.scrollY > 150) {
+    document.querySelector("#navbar").style.opacity = 0.9;
+  } else {
+    document.querySelector("#navbar").style.opacity = 1;
+  }
+});
+
+// Smooth scrolling
+$("#navbar a, .btn").on("click", function(event) {
+  if (this.hash !== "") {
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
