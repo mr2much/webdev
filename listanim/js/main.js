b689d549e9d3dc01d83ac2bd0ed8ef9cb9d14fcb
Anubis <anubis.lockward@gmail.com>
2019-05-15 10:33:33 -0400

    List elements can be added and removed

---
+++ listanim/js/main.js
@@ -0,0 +1,21 @@
+document.getElementById("add-to-list").onclick = function() {
+  var list = document.getElementById("list");
+  var newLI = document.createElement("li");
+  newLI.innerHTML = "A new item";
+  list.appendChild(newLI);
+
+  setTimeout(function() {
+    newLI.className = newLI.className + " show";
+  }, 10);
+};
+
+document.getElementById("list").onclick = function(event) {
+  var list = document.getElementById("list");
+
+  var li = event.target;
+  li.className = li.className + " hide";
+
+  setTimeout(function() {
+    list.removeChild(li);
+  }, 10);
+};
