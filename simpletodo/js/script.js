dc4f00ecfd1db26223309b9ada361e748d90c4aa
Anubis <anubis.lockward@gmail.com>
2019-10-14 02:21:35 -0400

    TODO can delete and improved list

--- simpletodo/js/script.js
+++ simpletodo/js/script.js
@@ -4,10 +4,19 @@ var input = prompt("What would you like to do?");
 
 while (input !== "quit") {
   if (input === "list") {
-    console.log(todos);
+    console.log("*********************************");
+    todos.forEach(function(todo, idx, arr) {
+      console.log(idx + ": " + todo);
+    });
+    console.log("*********************************");
   } else if (input === "new") {
     var newTodo = prompt("Enter new TODO");
+    console.log(newTodo + " added to the list");
     todos.push(newTodo);
+  } else if (input === "delete") {
+    var index = Number(prompt("Enter the index of the TODO to delete"));
+    todos.splice(index, 1);
+    console.log("TODO deleted");
   }
 
   input = prompt("What would you like to do?");
