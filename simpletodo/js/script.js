3ffbe20f4a7540c50365ada45ff57b62dbfd40da
Anubis <anubis.lockward@gmail.com>
2019-10-14 02:26:39 -0400

    Made code more modular

--- simpletodo/js/script.js
+++ simpletodo/js/script.js
@@ -4,22 +4,34 @@ var input = prompt("What would you like to do?");
 
 while (input !== "quit") {
   if (input === "list") {
-    console.log("*********************************");
-    todos.forEach(function(todo, idx, arr) {
-      console.log(idx + ": " + todo);
-    });
-    console.log("*********************************");
+    listTodos();
   } else if (input === "new") {
-    var newTodo = prompt("Enter new TODO");
-    console.log(newTodo + " added to the list");
-    todos.push(newTodo);
+    addNewTodo();
   } else if (input === "delete") {
-    var index = Number(prompt("Enter the index of the TODO to delete"));
-    todos.splice(index, 1);
-    console.log("TODO deleted");
+    deleteTodo();
   }
 
   input = prompt("What would you like to do?");
 }
 
 console.log("OK, YOU QUIT THE APP");
+
+function listTodos() {
+  console.log("*********************************");
+  todos.forEach(function(todo, idx, arr) {
+    console.log(idx + ": " + todo);
+  });
+  console.log("*********************************");
+}
+
+function addNewTodo() {
+  var newTodo = prompt("Enter new TODO");
+  console.log(newTodo + " added to the list");
+  todos.push(newTodo);
+}
+
+function deleteTodo() {
+  var index = Number(prompt("Enter the index of the TODO to delete"));
+  todos.splice(index, 1);
+  console.log("TODO deleted");
+}
