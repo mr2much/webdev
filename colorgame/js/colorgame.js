f1d210d111bdea323c7bb44173091a515a298693
Anubis <anubis.lockward@gmail.com>
2019-11-05 14:47:14 -0400

    Refactored code for the buttons

--- colorgame/js/colorgame.js
+++ colorgame/js/colorgame.js
@@ -7,76 +7,51 @@ var h1 = document.querySelector("h1");
 var resetBtn = document.querySelector("#reset");
 var easyBtn = document.querySelector("#easyBtn");
 var hardBtn = document.querySelector("#hardBtn");
+var modeButtons = document.querySelectorAll(".mode");
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 colorDisplay.textContent = pickedColor;
 
-resetBtn.addEventListener("click", function() {
+for (var i = 0; i < modeButtons.length; i++) {
+  modeButtons[i].addEventListener("click", function() {
+    modeButtons[0].classList.remove("selected");
+    modeButtons[1].classList.remove("selected");
+    this.classList.add("selected");
+
+    maxColors = this.textContent === "Easy" ? 3 : 6;
+
+    reset();
+  });
+}
+
+function reset() {
   // generate all new colors
   colors = generateRandomColors(maxColors);
 
   // pick a new rancom color from array
   pickedColor = pickColor();
   colorDisplay.textContent = pickedColor;
-  this.textContent = "New Colors";
+  resetBtn.textContent = "New Colors";
+  messageDisplay.textContent = "";
 
   // change color of squares
   for (var i = 0; i < squares.length; i++) {
     // add initial colors to squares
-    squares[i].style.backgroundColor = colors[i];
-
-    // add click listener to squares
-    squares[i].addEventListener("click", function() {
-      //grab color of clicked square
-      var clickedColor = this.style.backgroundColor;
-      //compare color to pickedColor
-      if (clickedColor === pickedColor) {
-        messageDisplay.textContent = "Correct!";
-        changeColors(clickedColor);
-      } else {
-        messageDisplay.textContent = "Try Again!";
-        this.style.backgroundColor = "#232323";
-      }
-    });
-  }
-
-  messageDisplay.textContent = "";
-  h1.style.backgroundColor = "steelblue";
-});
-
-easyBtn.addEventListener("click", function() {
-  easyBtn.classList.add("selected");
-  hardBtn.classList.remove("selected");
-  maxColors = 3;
-
-  colors = generateRandomColors(maxColors);
-  pickedColor = pickColor();
-  colorDisplay.textContent = pickedColor;
-
-  for (var i = 0; i < squares.length; i++) {
     if (colors[i]) {
+      squares[i].style.display = "block";
       squares[i].style.backgroundColor = colors[i];
     } else {
       squares[i].style.display = "none";
     }
   }
-});
 
-hardBtn.addEventListener("click", function() {
-  hardBtn.classList.add("selected");
-  easyBtn.classList.remove("selected");
+  h1.style.backgroundColor = "steelblue";
+}
 
-  maxColors = 6;
-  colors = generateRandomColors(maxColors);
-  pickedColor = pickColor();
-  colorDisplay.textContent = pickedColor;
-
-  for (var i = 0; i < squares.length; i++) {
-    squares[i].style.backgroundColor = colors[i];
-    squares[i].style.display = "block";
-  }
+resetBtn.addEventListener("click", function() {
+  reset();
 });
 
 for (var i = 0; i < squares.length; i++) {
