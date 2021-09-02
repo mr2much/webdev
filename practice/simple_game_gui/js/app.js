a77a14c40d037b8d2d5b13d07d888ab4da867424
Anubis Lockward <anubis.lockward@gmail.com>
2021-09-02 04:35:00 -0400

    Cleaned code

--- practice/simple_game_gui/js/app.js
+++ practice/simple_game_gui/js/app.js
@@ -1,14 +1,5 @@
-// knight = new Proxy(knight, {
-//   set(target, property, value) {
-//     console.log("Knight properties changed");
-//     target[property] = value;
-//     return true;
-//   },
-// });
-
 let div = document.createElement("div");
 let barraVida = new HPBar(knight);
-// const hpBar = barraVida.querySelector(`#${knight.name}`);
 
 div.appendChild(barraVida);
 document.body.appendChild(div);
@@ -17,6 +8,5 @@ div.addEventListener("click", () => {
   knight.hp--;
 
   barraVida._char.hp = knight.hp;
-
   console.log(knight.hp);
 });
--- practice/simple_game_gui/js/hp_bar.js
+++ practice/simple_game_gui/js/hp_bar.js
@@ -26,13 +26,17 @@ class HPBar extends HTMLElement {
   constructor(char) {
     super();
 
+    this.id = `${char.name}`;
+
     const hpBar = document.createElement("div");
     hpBar.classList.add("hp_bar");
 
     hpBar.innerHTML = hpBarTemplate(char);
 
+    const shadow = this.attachShadow({ mode: "open" });
+
     const self = this;
-    char = new Proxy(char, {
+    this._char = new Proxy(char, {
       set(target, property, value) {
         console.log("Changing", property, "to", value);
         target[property] = value;
@@ -41,43 +45,11 @@ class HPBar extends HTMLElement {
       },
     });
 
-    this._char = char;
-
-    const shadow = this.attachShadow({ mode: "open" });
-
     shadow.appendChild(hpBar);
   }
 
   updateView(char) {
     return hpBarTemplate(char);
-  }
-
-  connectedCallback() {
-    let char = this._char;
-    char = new Proxy(char, {
-      set(target, property, value) {
-        console.log("A property has changed");
-        target[property] = value;
-        return true;
-      },
-    });
-  }
-
-  attributeChangedCallback(name, oldVal, newVal) {
-    console.log("Something changed: ", name);
-  }
-
-  static get observedAttributes() {
-    return ["char"];
-  }
-
-  set char(value) {
-    console.log("Value set");
-    this._char = value;
-  }
-
-  get char() {
-    return this._char;
   }
 }
 
