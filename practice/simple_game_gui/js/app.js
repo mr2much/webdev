b36fdec7704976911d46f119a4d49b849763b166
Anubis Lockward <anubis.lockward@gmail.com>
2021-09-03 10:45:13 -0400

    Added simple observer notifier

--- practice/simple_game_gui/js/app.js
+++ practice/simple_game_gui/js/app.js
@@ -1,12 +1,24 @@
+// This would be the controller
+
+const hpObservers = [];
+
 let div = document.createElement("div");
 let barraVida = new HPBar(knight);
+
+hpObservers.push(barraVida);
 
 div.appendChild(barraVida);
 document.body.appendChild(div);
 
 div.addEventListener("click", () => {
   knight.hp--;
-
-  barraVida._char.hp = knight.hp;
+  
+  notifyObservers();
   console.log(knight.hp);
 });
+
+function notifyObservers() {
+  for(let i = 0; i < hpObservers.length; i++) {
+    hpObservers[i]._char.hp = knight.hp;
+  }
+}
