dc30e09d41d9f13b15233dc8c16464bca8effb47
Anubis Lockward <anubis.lockward@gmail.com>
2021-11-01 11:49:55 -0400

    Graphed global temp with chart.js

--- p5js/codingtrain/module1/global_temp/js/app.js
+++ p5js/codingtrain/module1/global_temp/js/app.js
@@ -1,59 +1,59 @@
-const button = document.querySelector("#myButton");
+const ctx = document.querySelector("#chart").getContext("2d");
 
-console.log(button);
+async function createChart() {
+  const data = await getData();
 
-// const myChart = new Chart(ctx, {
-//   type: "bar",
-//   data: {
-//     labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-//     datasets: [
-//       {
-//         label: "# of Votes",
-//         data: [12, 19, 3, 5, 2, 3],
-//         backgroundColor: [
-//           "rgba(255, 99, 132, 0.2)",
-//           "rgba(54, 162, 235, 0.2)",
-//           "rgba(255, 206, 86, 0.2)",
-//           "rgba(75, 192, 192, 0.2)",
-//           "rgba(153, 102, 255, 0.2)",
-//           "rgba(255, 159, 64, 0.2)",
-//         ],
-//         borderColor: [
-//           "rgba(255, 99, 132, 1)",
-//           "rgba(54, 162, 235, 1)",
-//           "rgba(255, 206, 86, 1)",
-//           "rgba(75, 192, 192, 1)",
-//           "rgba(153, 102, 255, 1)",
-//           "rgba(255, 159, 64, 1)",
-//         ],
-//         borderWidth: 1,
-//       },
-//     ],
-//   },
-//   options: {
-//     scales: {
-//       y: {
-//         beginAtZero: true,
-//       },
-//     },
-//   },
-// });
+  const myChart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: data.xs,
+      datasets: [
+        {
+          label: "Global Temperature",
+          data: data.ys,
+          fill: false,
+          backgroundColor: "rgba(255, 99, 132, 0.2)",
+          borderColor: "rgba(255, 99, 132, 1)",
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          ticks: {
+            callback: function (value, index, values) {
+              return `${value}°`;
+            },
+          },
+        },
+      },
+    },
+  });
+}
 
-// getData();
+createChart();
 
-// async function getData() {
-//   const res = await fetch("res/data/ZonAnn.Ts+dSST.csv");
+async function getData() {
+  const res = await fetch("res/data/ZonAnn.Ts+dSST.csv");
 
-//   const data = await res.text();
+  const xs = [];
+  const ys = [];
 
-//   const table = await data.split(/\r\n/);
+  const data = await res.text();
 
-//   table.forEach((row) => {
-//     const columns = row.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
+  const table = await data.split("\n");
 
-//     const year = columns[0];
-//     const temp = columns[1];
+  table.shift();
 
-//     console.log(`${year}, ${temp}`);
-//   });
-// }
+  table.forEach((row) => {
+    const columns = row.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
+
+    const year = columns[0];
+    xs.push(year);
+    const temp = columns[1];
+    ys.push(parseFloat(temp) + 14);
+  });
+
+  return { xs, ys };
+}
