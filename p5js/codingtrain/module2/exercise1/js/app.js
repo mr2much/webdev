29a26ef79d1ea4d78bec657c1f4d8340d829bee5
Anubis Lockward <anubis.lockward@gmail.com>
2021-11-02 23:15:58 -0400

    Completed code for extracting ATM data

--- p5js/codingtrain/module2/exercise1/js/app.js
+++ p5js/codingtrain/module2/exercise1/js/app.js
@@ -1,9 +1,39 @@
-retrieveData();
+async function showReport() {
+  const report = await retrieveData();
+
+  showDuplicates(report);
+
+  console.log(report);
+}
+
+async function showDuplicates(report) {
+  const reportMap = new Map();
+  const atms = report.atms;
+
+  for (let i = 0; i < atms.length; i++) {
+    if (reportMap.has(atms[i].atm)) {
+      const dup = reportMap.get(atms[i].atm);
+      let msg = document.createElement("p");
+      msg.textContent = JSON.stringify(dup);
+
+      let p = document.createElement("p");
+      p.textContent = JSON.stringify(atms[i]);
+
+      document.body.append(msg, p);
+    } else {
+      reportMap.set(atms[i].atm, atms[i]);
+    }
+  }
+}
+
+showReport();
 
 async function retrieveData() {
-  const res = await fetch("res/data/temp_data_atm.csv");
+  // const res = await fetch("res/data/temp_data_atm.csv");
+  // const res = await fetch("res/data/ReporteATM30-10-2021.csv");
+  const res = await fetch("res/data/ReporteATM04-09-2021.csv");
   const text = await res.text();
-  const data = await text.split("\r\n");
+  const data = text.split("\r\n");
 
   const reportDate = await extractReporteDate(data[2]);
   const categories = await extractCategories(data[3]);
@@ -15,10 +45,14 @@ async function retrieveData() {
     month: reportDate.month,
     year: reportDate.year,
     atms: atms,
-    total: atms.length,
+    count: atms.length,
   };
 
-  console.log(report);
+  const total = await getTotalAtms(report);
+
+  report.total = total;
+
+  return report;
 }
 
 async function extractReporteDate(data) {
@@ -42,16 +76,48 @@ async function extractCategories(data) {
 async function extractAtms(atms, categories) {
   let cajeros = [];
 
+  // this part extracts the atms
   for (let i = 0; i < atms.length; i++) {
     let temp = atms[i].split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
     let tempReport = {};
 
+    // this part matches the categories with their respective values from the temp variable after splitting the values in atms
     for (let j = 0; j < temp.length; j++) {
       tempReport[`${categories[j]}`] = temp[j];
     }
+
+    const dateTime = await getDateTime(tempReport);
+
+    const { fecha, hora = "0:00" } = dateTime;
+
+    // TODO: Look for simpler way of doing this
+    tempReport.fecha = fecha;
+    tempReport.hora = hora;
 
     cajeros.push(tempReport);
   }
 
   return cajeros;
+}
+
+async function getDateTime({ fecha }) {
+  const fullDate = fecha.split(" ");
+  let date = fullDate[0];
+  let hora = fullDate[1].split(":")[0];
+
+  hora = `${hora}:00`;
+
+  return { fecha: date, hora };
+}
+
+// Returns the number of unique atms
+// TODO: Find better solution
+async function getTotalAtms({ atms }) {
+  const atmMap = new Map();
+
+  for (let atm of atms) {
+    atmMap.set(atm.atm, atm);
+  }
+
+  return atmMap.size;
 }
