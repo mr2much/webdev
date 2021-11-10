52338aedf2b8ac8d2724e219de8c29b6bda56a13
Anubis Lockward <anubis.lockward@gmail.com>
2021-11-09 23:54:18 -0400

    Finished ISS tracker project on CodingTrain

--- p5js/codingtrain/module3/js/app.js
+++ p5js/codingtrain/module3/js/app.js
@@ -2,7 +2,7 @@ const iss_url = "https://api.wheretheiss.at/v1/satellites/25544";
 
 // Making map and tiles
 
-const mymap = L.map("issmap").setView([0, 0], 1);
+const mymap = L.map("issmap").setView([0, 0], 5);
 const attribution =
   'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
@@ -19,20 +19,38 @@ const myIcon = L.icon({
 
 const marker = L.marker([0, 0], { icon: myIcon }).addTo(mymap);
 
+let firstRun = true;
 async function getISS() {
   const res = await fetch(iss_url);
   const data = await res.json();
 
   const { latitude, longitude } = data;
 
+  let altitude = Math.random() * 1000;
+  const aspect = 1.5625;
+  const w = (altitude * aspect) / (altitude / 100);
+  const h = altitude / (altitude / 100);
+
+  myIcon.options.iconSize = [w, h];
+  myIcon.options.iconAnchor = [w / 2, h / 2];
+
   const lat = document.querySelector("#lat");
   const lon = document.querySelector("#lon");
 
-  //   L.marker([latitude, longitude]).addTo(mymap);
-  marker.setLatLng([latitude, longitude]);
+  // Set the view the first time this is run
+  if (firstRun) {
+    mymap.setView([latitude, longitude], mymap.getZoom());
+    firstRun = false;
+  }
 
-  lat.textContent = latitude;
-  lon.textContent = longitude;
+  marker.setLatLng([latitude, longitude]);
+  marker.setIcon(myIcon);
+
+  lat.textContent = latitude.toFixed(2);
+  lon.textContent = longitude.toFixed(2);
+  document.querySelector("#altitude").textContent = altitude.toFixed(2);
 }
 
 getISS();
+
+setInterval(getISS, 1000);
