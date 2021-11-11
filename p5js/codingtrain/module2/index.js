96a618cf01f5d5df8b8e02017504803d955a8a11
Anubis Lockward <anubis.lockward@gmail.com>
2021-11-11 19:50:59 -0400

    Implemented client/server communication

--- p5js/codingtrain/module2/index.js
+++ p5js/codingtrain/module2/index.js
@@ -5,3 +5,16 @@ app.listen("3000", () => {
 });
 
 app.use(express.static("public"));
+app.use(express.json({ limit: "1mb" }));
+
+app.post("/api", (req, res) => {
+  console.log("I got a request!");
+  console.log(req.body);
+
+  const data = req.body;
+  res.json({
+    status: "success",
+    latitude: data.lat,
+    longitude: data.lon,
+  });
+});
