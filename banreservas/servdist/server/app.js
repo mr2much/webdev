2387841015b6b74349f56e100f941353c5432b52
Anubis Lockward <anubis.lockward@gmail.com>
2021-12-17 20:06:02 -0400

    Setting server and routes.

--- banreservas/servdist/server/app.js
+++ banreservas/servdist/server/app.js
@@ -1,41 +1,48 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const fetch = require("node-fetch");
+const Datastore = require("nedb");
+const fs = require("fs");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
+const __dir = "./db";
+const __db = "servidoresxunidadgssd.db";
+const dbFilePath = path.join(__dir, __db);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
+app.use(logger("dev"));
+app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const database = new Datastore(dbFilePath);
+database.loadDatabase();
+app.get("/", async (req, res, next) => {
+  database.find({}, (err, entries) => {
+    if (err) {
+      next(err);
+    }
+
+    res.json(entries);
+  });
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+app.use(function (err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    message: err.message,
+    error: req.app.get("env") === "development" ? err.stack : {},
+  });
 });
 
 module.exports = app;
