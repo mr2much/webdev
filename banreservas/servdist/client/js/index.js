445d3da99710857d4939200d1c71842b3684d524
Anubis Lockward <anubis.lockward@gmail.com>
2021-12-26 21:47:34 -0400

    Added form validations and feedback

--- banreservas/servdist/client/js/index.js
+++ banreservas/servdist/client/js/index.js
@@ -1,5 +1,3 @@
-const API_URL = "http://localhost:3000/api/v1/servers";
-
 const searchBar = document.querySelector("#search");
 const ulServer = document.querySelector("#matches");
 
@@ -84,10 +82,16 @@ function getHTMLString(server) {
                 <h5 class="card-header"><strong>Servidor: </strong><span class="server">${server.server}</span></h5>
                 <div class="card-body flex-column">                  
                   <p class="card-text"><strong>DNS: </strong><span class="dns">${server.dns}</span></p>
-                  <p><strong>Función: </strong><span class="function">${server.function}</span></p>                  
+                  <p><strong>Función: </strong><span class="function">${server.func}</span></p>                  
                   <p><strong>IP: </strong><span class="ip">${server.ip}</span></p>
                   <p><strong>Soporte OS (Memoria, Discos, CPU): </strong><span class="os_support">${server.os_support}</span></p>
                   <p><strong>Soporte Aplicación: </strong><span class="app_support">${server.app_support}</span></p>
+                  <p><strong>Notas: </strong><span class="notes">${server.notes}</
+                  span></p>
+                  <p><strong>Fecha de Creación: </strong><span class="date_created">${server.date_created}</
+                  span></p>
+                  <p><strong>Ultima Modificación: </strong><span class="last_modified">${server.last_modified}</
+                  span></p>
                 </div>
             </div>
             <div>
--- banreservas/servdist/server/db/queries.js
+++ banreservas/servdist/server/db/queries.js
@@ -23,4 +23,36 @@ module.exports = {
       });
     });
   },
+  create(server) {
+    return new Promise(async (resolve, reject) => {
+      console.log("Received a request to create an entry");
+
+      const res = await this.getOne(server.id);
+
+      console.log(res);
+
+      // this.getOne({ server: data.server }, (err, server) => {
+      //   if (err) {
+      //     console.log("Error");
+      //     res.json({ success: false, statusCode: 500, message: err });
+      //     res.end();
+      //     return;
+      //   }
+
+      //   if (server) {
+      //     res.json({ success: false, message: "duplicated" });
+      //   } else {
+      //     database.insert(data, (err, newDoc) => {
+      //       if (err) {
+      //         res.json({ success: false, statusCode: 500, message: err });
+      //         res.end();
+      //         return;
+      //       }
+
+      //       res.json(newDoc);
+      //     });
+      //   }
+      // });
+    });
+  },
 };
