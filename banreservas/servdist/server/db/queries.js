1e4e14681a571d684ec0fab9fc29603a4267431a
Anubis Lockward <anubis.lockward@gmail.com>
2021-12-29 04:30:11 -0400

    Add create product route and queries

--- banreservas/servdist/server/db/queries.js
+++ banreservas/servdist/server/db/queries.js
@@ -25,47 +25,22 @@ module.exports = {
   },
   create(server) {
     return new Promise(async (resolve, reject) => {
-      console.log("Received a request to create an entry");
-
       const res = await this.getOne(server);
 
       // if res is not null it means the entry exists
       if (res) {
-        const error = `${server.server} already exists!`;
+        const error = new Error(`${server.server} already exists!`);
         reject(error);
       } else {
         // save entry to DB
-        // connection.insert(server, (err, newDoc) => {
-        //   if(err) {
-        //     reject(err);
-        //   }
-        // });
-        console.log(server);
-        console.log("Saved correctly");
+        connection.insert(server, (err, newDoc) => {
+          if (err) {
+            reject(err);
+          }
+
+          resolve(newDoc);
+        });
       }
-
-      // this.getOne({ server: data.server }, (err, server) => {
-      //   if (err) {
-      //     console.log("Error");
-      //     res.json({ success: false, statusCode: 500, message: err });
-      //     res.end();
-      //     return;
-      //   }
-
-      //   if (server) {
-      //     res.json({ success: false, message: "duplicated" });
-      //   } else {
-      //     database.insert(data, (err, newDoc) => {
-      //       if (err) {
-      //         res.json({ success: false, statusCode: 500, message: err });
-      //         res.end();
-      //         return;
-      //       }
-
-      //       res.json(newDoc);
-      //     });
-      //   }
-      // });
     });
   },
 };
