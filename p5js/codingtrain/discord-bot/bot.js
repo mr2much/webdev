94d9061dfb4dcc2f6d0a4b31081a9684cbef521d
Anubis Lockward <anubis.lockward@gmail.com>
2022-07-01 18:38:26 -0400

    Bot only replies on bot_testing channel

--- p5js/codingtrain/discord-bot/bot.js
+++ p5js/codingtrain/discord-bot/bot.js
@@ -1,3 +1,5 @@
+const clientID = "991610668992176158";
+
 const { Client, Intents, Interaction } = require("discord.js");
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
@@ -14,7 +16,7 @@ client.on("ready", () => {
 client.on("messageCreate", async (msg) => {
   console.log(`Message received: ${msg}`);
 
-  if (msg.content === "choo choo") {
+  if (msg.channel.id === clientID && msg.content === "choo choo") {
     msg.reply(":heart:");
   }
 });
