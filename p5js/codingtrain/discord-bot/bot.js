511b5bc5fd94b359462f44f8bb74cfe189485d27
Anubis Lockward <anubis.lockward@gmail.com>
2022-07-01 18:35:31 -0400

    Bot is replying to messages that say choo choo

--- p5js/codingtrain/discord-bot/bot.js
+++ p5js/codingtrain/discord-bot/bot.js
@@ -1,21 +1,24 @@
-const Discord = require("discord.js");
-const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS] });
-
-client.on("ready", () => {
-  console.log(`logged in as ${client.user.tag}`);
+const { Client, Intents, Interaction } = require("discord.js");
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
 client.login(
   "OTkxNjA0ODkzMDc0ODUzODg4.GypmHw.1tq8Lxwydog3laSsaClZyWkrt3iYzqZ5G47Jk4"
 );
 
-// const { Client, GatewayIntentBits } = require("discord.js");
-// const client = new Client({ intents: [GatewayIntentBits] });
+client.on("ready", () => {
+  console.log(`logged in as ${client.user.tag}`);
+});
 
-// client.on("ready", () => {
-//   console.log(`Logged in as ${client.user.tag}`);
-// });
+client.on("messageCreate", async (msg) => {
+  console.log(`Message received: ${msg}`);
 
-// client.login(
-//   "OTkxNjA0ODkzMDc0ODUzODg4.GypmHw.1tq8Lxwydog3laSsaClZyWkrt3iYzqZ5G47Jk4"
-// );
+  if (msg.content === "choo choo") {
+    msg.reply(":heart:");
+  }
+});
+
+client.on("interactionCreate", async (interaction) => {
+  console.log(`Interaction Received ${interaction}`);
+});
