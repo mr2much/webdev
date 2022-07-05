ae924edc5b03583bb6f64bb1aebb4c8be57d2120
Anubis Lockward <anubis.lockward@gmail.com>
2022-07-05 16:36:13 -0400

    Implemented command handler

--- p5js/codingtrain/discord-bot/src/bot.js
+++ p5js/codingtrain/discord-bot/src/bot.js
@@ -1,12 +1,6 @@
 // const clientID = '991610668992176158';
-const replies = [
-  'The answer is 42',
-  ':heart:',
-  ':dizzy:',
-  'These are not the droids you are looking for',
-];
-
 require('dotenv').config();
+const commandHandler = require('./commands');
 
 const { Client, Intents } = require('discord.js');
 
@@ -20,15 +14,7 @@ client.on('ready', () => {
   console.log(`logged in as ${client.user.tag}`);
 });
 
-client.on('messageCreate', async (msg) => {
-  console.log(`Message received: ${msg}`);
-
-  if (msg.channel.id === process.env.CLIENT_ID && msg.content === 'choo choo') {
-    // msg.reply(":heart:");
-    const index = Math.floor(Math.random() * replies.length);
-    msg.channel.send(replies[index]);
-  }
-});
+client.on('messageCreate', commandHandler);
 
 client.on('interactionCreate', async (interaction) => {
   console.log(`Interaction Received ${interaction}`);
