d841065b1f5a323ff918b50a6103f4f2a78762a6
Anubis Lockward <anubis.lockward@gmail.com>
2022-12-03 00:57:17 -0400

    Add code to populate edit form

--- hhrr_app/public/edit/js/edit.js
+++ hhrr_app/public/edit/js/edit.js
@@ -1,3 +1,14 @@
 /* eslint-disable linebreak-style */
 const idCandidato = parseIDFromURL();
 const form = document.querySelector('form');
+
+function fillFormWithCandidato(candidato) {
+  document.querySelector('#cedula').value = candidato.cedula;
+  document.querySelector('#nombres').value = candidato.nombres;
+  document.querySelector('#apellidos').value = candidato.apellidos;
+  document.querySelector('#dob').value = candidato.dob;
+  document.querySelector('#job_actual').value = candidato.job_actual;
+  document.querySelector('#salary_exp').value = candidato.exp_salario;
+}
+
+getCandidato(idCandidato).then(fillFormWithCandidato);
