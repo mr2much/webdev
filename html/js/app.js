c8281cea2b357a7ea513dcddeff964acf1d7d134
Anubis Lockward <anubis.lockward@gmail.com>
2024-07-03 18:05:11 -0400

    Start adding code to create carousel info

--- html/js/app.js
+++ html/js/app.js
@@ -1,3 +1,6 @@
+const catFactList = document.querySelector('#cat-fact-list');
+const catFactContainer = document.querySelector('#container-cat-facts');
+
 const BASE_URL = 'https://catfact.ninja/facts';
 let catFacts = [];
 
@@ -6,11 +9,23 @@ const loadEntries = async () => {
   catFacts = res.data.data.map((info) => info.fact);
 };
 
+{
+  /* <li
+                data-target="#carouselCatFacts"
+                data-slide-to="0"
+                class="active"
+              ></li> */
+}
 const showEntries = () => {
   if (catFacts.length > 0) {
-    catFacts.forEach((fact) => {
-      console.log(fact);
-    });
+    for (let i = 0; i < catFacts.length; i++) {
+      const newIndicatorLI = document.createElement('li');
+      newIndicatorLI.setAttribute('data-target', '#carouselCatFacts');
+      newIndicatorLI.setAttribute('data-slide-to', `${i}`);
+    }
+    // catFacts.forEach((fact) => {
+    //   console.log(fact);
+    // });
   }
 };
 
