066679be63a61709a95befebd233baa9e7d32ca8
Anubis Lockward <anubis.lockward@gmail.com>
2024-07-04 23:54:16 -0400

    Finish working with Cat Facts carousel

--- html/js/app.js
+++ html/js/app.js
@@ -1,4 +1,3 @@
-const catFactList = document.querySelector('#cat-fact-list');
 const catFactContainer = document.querySelector('#container-cat-facts');
 
 const BASE_URL = 'https://catfact.ninja/facts';
@@ -9,23 +8,27 @@ const loadEntries = async () => {
   catFacts = res.data.data.map((info) => info.fact);
 };
 
-{
-  /* <li
-                data-target="#carouselCatFacts"
-                data-slide-to="0"
-                class="active"
-              ></li> */
-}
 const showEntries = () => {
   if (catFacts.length > 0) {
     for (let i = 0; i < catFacts.length; i++) {
-      const newIndicatorLI = document.createElement('li');
-      newIndicatorLI.setAttribute('data-target', '#carouselCatFacts');
-      newIndicatorLI.setAttribute('data-slide-to', `${i}`);
+      const newDiv = document.createElement('div');
+
+      newDiv.classList.add('carousel-item');
+
+      if (i === 0) {
+        newDiv.classList.add('active');
+      }
+
+      newDiv.innerHTML = `
+        <div class="carousel-caption d-none d-md-block">
+            <p>
+              ${catFacts[i]}
+            </p>
+        </div>
+      `;
+
+      catFactContainer.append(newDiv);
     }
-    // catFacts.forEach((fact) => {
-    //   console.log(fact);
-    // });
   }
 };
 
